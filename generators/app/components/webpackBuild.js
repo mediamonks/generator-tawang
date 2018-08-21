f80b6c67bf91ea7d7f35dccd71e539437510c588
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-21 11:47:56 +0200

    Refactor webpack build module

--- generators/app/components/webpackBuild.js
+++ generators/app/components/webpackBuild.js
@@ -2,23 +2,14 @@ const { spawn } = require('child_process');
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    // const child = spawn('npm', ['run', 'build'], {stdio: "inherit"});
-    const child = spawn('npm', ['run', 'build'], { customFds: [0, 1, 2] });
-
-    /* // use child.stdout.setEncoding('utf8'); if you want text chunks
-  child.stdout.on('data', chunk => {
-    // data from standard output is here as buffers
-
-    this.log(chunk.toString())
-  }); 
-
-  // since these are streams, you can pipe them elsewhere
-  //child.stderr.pipe(dest);
-  child.stdout.pipe(process.stdout); */
+    const child = spawn('npm', ['run', 'build'], { stdio: 'inherit' });
 
     child.on('close', code => {
-      console.log(`child process exited with code ${code}`);
-      resolve();
+      if (code === 0) {
+        resolve();
+      } else {
+        reject();
+      }
     });
   });
 };
